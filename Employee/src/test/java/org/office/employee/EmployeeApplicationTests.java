96b5ef828b1305b71dad064280262d52a11fa02d
Bhargav <bhargav@email.com>
2025-10-22 21:49:32 +0530

    vwd

---
+++ Employee/src/test/java/org/office/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.office.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
